a7c826acad4085e771c4a82f9912ea1efddb704e
Jagger <634750802@qq.com>
2023-05-01 23:59:19 +0800

    fix

--- packages/buildtool/webpack/plugins/MySQLPlugin.ts
+++ packages/buildtool/webpack/plugins/MySQLPlugin.ts
@@ -1,21 +1,19 @@
 import { Compiler, WebpackPluginInstance } from 'webpack';
 import { createPool, Pool } from 'mysql2/promise';
-import { parse as parseDotEnv } from 'dotenv';
-import * as fs from 'fs';
 import Cache from '../utils/cache';
-import { cwd } from '../utils/path';
+import { getEnv } from '../utils/env';
 
 const db: {
   pool: Pool
   cache: Cache
 } = {} as any;
 
-const PLUGIN_NAME = 'MySQLPlugin'
+const PLUGIN_NAME = 'MySQLPlugin';
 
 class MySQLPlugin implements WebpackPluginInstance {
   apply (compiler: Compiler) {
     compiler.hooks.beforeCompile.tap(PLUGIN_NAME, async () => {
-      const { DATABASE_URL } = parseDotEnv(fs.readFileSync(cwd('.env.local')));
+      const DATABASE_URL = getEnv('DATABASE_URL');
       if (!db.pool) {
         db.pool = createPool(DATABASE_URL as string);
         db.cache = new Cache('queries', '.db');
---
+++ packages/buildtool/webpack/utils/env.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import * as fs from 'fs';
+import { DotenvParseOutput, parse } from 'dotenv';
+
+let env: DotenvParseOutput | undefined;
+
+export function parseDotEnv (dir: string) {
+  return ['.env.local', `.env.${process.env.NODE_ENV}`, '.env']
+    .map(fn => path.join(dir, fn))
+    .filter(fs.existsSync)
+    .map(fn => parse(fs.readFileSync(fn, { encoding: 'utf-8' })))
+    .reduce((curr, obj) => {
+      return Object.assign(curr, obj);
+    }, {});
+}
+
+export function getEnv (name: string): string | undefined {
+  if (env == null) {
+    env = parseDotEnv(process.cwd());
+  }
+  return process.env[name] ?? env[name];
+}
