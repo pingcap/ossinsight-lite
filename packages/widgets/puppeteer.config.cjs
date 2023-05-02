12e314d146abc615f1dd89d5b71a60d85e302db9
Jagger <634750802@qq.com>
2023-05-02 16:01:55 +0800

    remove arbitrary config

--- packages/widgets/puppeteer.config.cjs
+++ packages/widgets/puppeteer.config.cjs
@@ -2,5 +2,4 @@ const {join} = require('path');
 
 module.exports = {
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  executablePath: join(__dirname, '.cache/puppeteer/chrome/Chromium.app/Contents/MacOS/Chromium'),
 };
