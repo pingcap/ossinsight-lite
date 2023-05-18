5686e5ade5549970b290da1de9dd42edd22b82f3
Jagger <634750802@qq.com>
2023-05-18 17:48:08 +0800

    fix type

--- src/utils/clientOnly.ts
+++ src/utils/clientOnly.ts
@@ -2,5 +2,5 @@ import dynamic from 'next/dynamic';
 import { ComponentType } from 'react';
 
 export default function clientOnly<P> (Component: ComponentType<P>): ComponentType<P> {
-  return dynamic(() => Promise.resolve(Component), { ssr: false });
+  return dynamic(() => Promise.resolve(Component), { ssr: false }) as any;
 }
