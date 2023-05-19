cd6de97aeb492b2c263950b5588e7cd5fdc96a8e
Jagger <634750802@qq.com>
2023-05-19 19:10:34 +0800

    fix

--- app/dashboards/page.tsx
+++ app/dashboards/page.tsx
@@ -1,5 +1,18 @@
-import Dashboards from '@/src/_pages/Dashboards/index';
+import Create from '@/src/_pages/Dashboards/Create';
+import List from '@/src/_pages/Dashboards/List';
+import { serverDashboardNames } from '@/app/api/layout/operations.server';
 
-export default function () {
-  return <Dashboards />;
+export default async function () {
+  const [_, dashboards] = await serverDashboardNames();
+
+  return (
+    <div className="container m-auto p-4">
+      <ul>
+        <List dashboards={dashboards} />
+        <li>
+          <Create />
+        </li>
+      </ul>
+    </div>
+  );
 }
