10fe671e8573ae277b464ab25961dce4377260d3
Jagger <634750802@qq.com>
2023-05-19 19:08:20 +0800

    fix

--- app/api/layout.json/route.ts
+++ app/api/layout.json/route.ts
@@ -2,7 +2,7 @@ import { getAllDashboards, getLibrary } from '@/app/api/layout/operations';
 import { NextResponse } from 'next/server';
 import { LayoutConfigV1 } from '@/src/types/config';
 
-export async function GET (req: NextResponse) {
+export async function GET () {
   const [[libraryStore, library], [dashboardsStore, dashboard]] = await Promise.all([getLibrary(), getAllDashboards()]);
 
   const config: LayoutConfigV1 = {
