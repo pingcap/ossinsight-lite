e975b911d6202d588dd4ca6bccf266f506e2b353
Jagger <634750802@qq.com>
2023-05-25 16:13:08 +0800

    public layout.json api

--- app/(client)/api/layout.json/route.ts
+++ app/(client)/api/layout.json/route.ts
@@ -1,13 +1,13 @@
 import { getAllDashboards, getLibrary } from '@/app/(client)/api/layout/operations';
 import { NextRequest, NextResponse } from 'next/server';
 import { LayoutConfigV1 } from '@/src/types/config';
-import { authenticateApiGuard } from '@/src/auth';
+// import { authenticateApiGuard } from '@/src/auth';
 
 export async function GET (req: NextRequest) {
-  const res = await authenticateApiGuard(req);
-  if (res) {
-    return res;
-  }
+  // const res = await authenticateApiGuard(req);
+  // if (res) {
+  //   return res;
+  // }
 
   const [libraryStore, library] = await getLibrary();
   const [dashboardsStore, dashboard] = await getAllDashboards();
