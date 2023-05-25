e8faa3684a535f4398f134fb3181561b558d6728
Jagger <634750802@qq.com>
2023-05-25 15:01:58 +0800

    fix blank dashboard when switching pages

--- app/(client)/(dashboards)/dashboard.client.tsx
+++ app/(client)/(dashboards)/dashboard.client.tsx
@@ -2,6 +2,7 @@
 
 import { commands, currentDashboard } from '@/app/bind';
 import { singletons, whenReady } from '@/packages/ui/hooks/bind';
+import { useWhenReady } from '@/packages/ui/hooks/bind/hooks';
 import Dashboard from '@/src/_pages/Dashboard';
 import { defaultLayoutConfig } from '@/src/components/WidgetsManager/defaults';
 import { Dashboard as DashboardConfig } from '@/src/types/config';
@@ -11,6 +12,24 @@ import React, { useEffect } from 'react';
 export default function DashboardClient () {
   const [, name = 'default', mode] = useSelectedLayoutSegments();
   const editing = mode === 'edit';
+
+  useWhenReady(singletons, 'dashboard', (canvas, sub) => {
+    sub.add(currentDashboard.subscribe(next => {
+      if (next) {
+        commands.inactiveScope(() => {
+          canvas.current.syncWith(next);
+        });
+      }
+    }));
+
+    if (currentDashboard.current) {
+      canvas.current.syncWith(currentDashboard.current);
+    }
+
+    sub.add(() => {
+      canvas.current.dispose();
+    });
+  }, []);
 
   useEffect(() => {
     const sub = whenReady(singletons, 'dashboard', (canvas, sub) => {
