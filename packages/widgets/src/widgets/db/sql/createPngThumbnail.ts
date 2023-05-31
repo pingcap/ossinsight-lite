b31849c87ba785f80a1b7dcd5fd4bf4687360d9c
Jagger <634750802@qq.com>
2023-05-31 16:05:18 +0800

    support render line chart to png

--- packages/widgets/src/widgets/db/sql/createPngThumbnail.ts
+++ packages/widgets/src/widgets/db/sql/createPngThumbnail.ts
@@ -7,28 +7,50 @@ import { titlePlugin } from '../../../components/visualize/chartjs/titlePlugin';
 import { WidgetProps } from './Widget';
 
 export default async function (server: ServerContext, props: WidgetProps, ctx: CanvasRenderingContext2D) {
-  if (props.visualize.type !== 'chart:bar') {
-    throw new Error(`${props.visualize.type} not support yet`);
+  if (props.visualize.type === 'table') {
+    throw new Error(`Rendering table to PNG is not support yet`);
+  }
+
+  if (props.visualize.type === 'gauge') {
+    throw new Error(`Rendering gauge to PNG is not support yet`);
   }
 
   const data = await server.runSql(props.currentDb, props.sql);
 
   const { x, y, title } = props.visualize;
 
-  new ChartJs(ctx, {
-    data: { datasets: [{ type: 'bar', ...barDataset(data, x, y) }] },
-    options: {
-      animation: false,
-      maintainAspectRatio: false,
-      indexAxis: (['x', 'y'] as const)[[x, y].findIndex(axis => axis.type !== 'value') ?? 0] ?? 'x',
-      scales: {
-        x: getCartesianScaleOption(data, x, 'x'),
-        y: getCartesianScaleOption(data, y, 'y'),
+  if (props.visualize.type !== 'chart:bar') {
+    new ChartJs(ctx, {
+      data: { datasets: [{ type: 'bar', ...barDataset(data, x, y) }] },
+      options: {
+        animation: false,
+        maintainAspectRatio: false,
+        indexAxis: (['x', 'y'] as const)[[x, y].findIndex(axis => axis.type !== 'value') ?? 0] ?? 'x',
+        scales: {
+          x: getCartesianScaleOption(data, x, 'x'),
+          y: getCartesianScaleOption(data, y, 'y'),
+        },
+        plugins: {
+          title: titlePlugin(title),
+          legend: legendsPlugin(),
+        },
       },
-      plugins: {
-        title: titlePlugin(title),
-        legend: legendsPlugin(),
+    });
+  } else {
+    new ChartJs(ctx, {
+      data: { datasets: [{ type: 'line', ...barDataset(data, x, y) }] },
+      options: {
+        animation: false,
+        maintainAspectRatio: false,
+        scales: {
+          x: getCartesianScaleOption(data, x, 'x'),
+          y: getCartesianScaleOption(data, y, 'y'),
+        },
+        plugins: {
+          title: titlePlugin(title),
+          legend: legendsPlugin(),
+        },
       },
-    },
-  });
+    });
+  }
 }
