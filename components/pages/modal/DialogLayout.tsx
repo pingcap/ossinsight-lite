04d7cf79a33da7ccde9918810fac256474ab5623
Jagger <634750802@qq.com>
2023-06-02 14:55:43 +0800

    dialog animation

--- components/pages/modal/DialogLayout.tsx
+++ components/pages/modal/DialogLayout.tsx
@@ -1,28 +1,46 @@
 'use client';
 import { ModalContext } from '@/app/@modal/(all)/context';
 import AppLoading from '@/components/AppLoading';
+import useRefCallback from '@/packages/ui/hooks/ref-callback';
 import * as Dialog from '@radix-ui/react-dialog';
 import { useRouter } from 'next/navigation';
-import { Suspense, useCallback } from 'react';
+import { Suspense, useCallback, useEffect, useState } from 'react';
 
 export default function DialogLayout ({ children }: any) {
   const router = useRouter();
+  const [open, setOpen] = useState(true);
+  const [animation, setAnimation] = useState(false);
+
+  const handleAnimationEnd = useRefCallback((ev) => {
+    setAnimation(false);
+  });
+
+  useEffect(() => {
+    if (!open && !animation) {
+      router.back();
+    }
+  }, [animation, open]);
 
   const closeModal = useCallback(() => {
-    router.back();
+    setAnimation(true);
+    setOpen(false);
   }, []);
 
-  const onOpenChange = useCallback((open: boolean) => {
+  const onOpenChange = useRefCallback((open: boolean) => {
+    setOpen(open);
     if (!open) {
-      closeModal();
+      setAnimation(true);
     }
-  }, []);
+  });
 
   return (
-    <Dialog.Root open onOpenChange={onOpenChange}>
-      <Dialog.Portal>
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      <Dialog.Portal forceMount={animation ? true : undefined}>
         <Dialog.Overlay className="site-modal-overlay" />
-        <Dialog.Content className="site-modal-content">
+        <Dialog.Content
+          className="site-modal-content"
+          onAnimationEndCapture={handleAnimationEnd}
+        >
           <ModalContext.Provider value={{ closeModal }}>
             <Suspense fallback={<AppLoading />}>
               {children}
