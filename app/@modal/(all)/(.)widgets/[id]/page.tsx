7a8241948ca7817d6d3bfd2a79efaef419136514
Jagger <634750802@qq.com>
2023-06-05 18:33:10 +0800

    fix

--- app/@modal/(all)/(.)widgets/[id]/page.tsx
+++ app/@modal/(all)/(.)widgets/[id]/page.tsx
@@ -1,18 +1,26 @@
-import { findItem } from '@/actions/widgets';
+import { getLibraryItem } from '@/app/(client)/api/layout/operations';
+import { Unauthorized } from '@/components/Errors';
 import WidgetPreviewWithDetails from '@/components/pages/widget/WidgetPreviewWithDetails';
+import { isReadonly } from '@/utils/server/auth';
 import { notFound } from 'next/navigation';
 
 export default async function ({ params }: any) {
   const id = decodeURIComponent(params.id);
 
-  const item = await findItem(id);
+  const item = await getLibraryItem(id);
 
   if (!item) {
     notFound();
   }
 
+  if (isReadonly() && item.visibility !== 'public') {
+    return (
+      <Unauthorized />
+    );
+  }
+
   return (
-    <div className='h-full flex items-center overflow-hidden'>
+    <div className="h-full flex items-center overflow-hidden">
       <WidgetPreviewWithDetails item={item} />
     </div>
   );
