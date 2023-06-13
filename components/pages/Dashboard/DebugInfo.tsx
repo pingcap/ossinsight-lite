90dad5a04d957d065ce0eeef1cbca0afd88e1890
Jagger <634750802@qq.com>
2023-06-13 14:03:46 +0800

    conditional show debug info

--- components/pages/Dashboard/DebugInfo.tsx
+++ components/pages/Dashboard/DebugInfo.tsx
@@ -1,13 +1,22 @@
 import { State } from '@/store/store';
 import clientOnly from '@/utils/clientOnly';
+import { useMemo } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 
 function DebugInfo () {
+  const showDebugInfo = useMemo(() => {
+    return localStorage.getItem('ossinsight-lite.debug') === 'true';
+  }, []);
+
   const { dirty, committing, local } = useSelector<State, { dirty: number, committing: number, local: number }>(({ draft }) => ({
     dirty: draft.dirty.length,
     committing: draft.committing.length,
     local: draft.localStorageUncommittedChanges.length,
   }), shallowEqual);
+
+  if (!showDebugInfo) {
+    return <></>;
+  }
 
   return (
     <div className="fixed right-0 bottom-0 z-50 font-sans text-sm text-gray-400">
