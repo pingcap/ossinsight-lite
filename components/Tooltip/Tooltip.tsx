946019448a0a9e6edd2eee68f52c3d9162b0dc87
Jagger <634750802@qq.com>
2023-06-14 14:17:10 +0800

    add tooltip

---
+++ components/Tooltip/Tooltip.tsx
@@ -0,0 +1,25 @@
+import * as RuiTooltip from '@radix-ui/react-tooltip';
+import { ReactElement, ReactNode } from 'react';
+
+export interface TooltipProps {
+  label: ReactNode;
+  children: ReactElement;
+}
+
+export default function Tooltip ({ children, label }: TooltipProps) {
+  return (
+    <RuiTooltip.Provider delayDuration={0}>
+      <RuiTooltip.Root>
+        <RuiTooltip.Trigger asChild>
+          {children}
+        </RuiTooltip.Trigger>
+        <RuiTooltip.Portal>
+          <RuiTooltip.Content className='tooltip-content' sideOffset={5}>
+            <RuiTooltip.Arrow className="fill-white" />
+            {label}
+          </RuiTooltip.Content>
+        </RuiTooltip.Portal>
+      </RuiTooltip.Root>
+    </RuiTooltip.Provider>
+  );
+}
