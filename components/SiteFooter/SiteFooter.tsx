b73861d7bea1942ae49a2a64b4d84bfdc462eb4f
Jagger <634750802@qq.com>
2023-06-20 17:22:41 +0800

    ui: add footer

---
+++ components/SiteFooter/SiteFooter.tsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export function SiteFooter () {
+  return  (
+    <footer className='site-footer'>
+      Powered by <a href='https://github.com/pingcap/ossinsight-lite' target='_blank'>pingcap/ossinsight-lite</a>
+    </footer>
+  )
+}
