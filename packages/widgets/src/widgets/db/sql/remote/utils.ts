f213720610a42c2d376b90b86f322b51b850e79a
Jagger <634750802@qq.com>
2023-06-25 15:21:28 +0800

    optimize(ssr): improve ssr metadata info generation

--- packages/widgets/src/widgets/db/sql/remote/utils.ts
+++ packages/widgets/src/widgets/db/sql/remote/utils.ts
@@ -1,4 +1,3 @@
-
 export interface RemoteInfo {
   owner: string;
   repo: string;
@@ -6,15 +5,19 @@ export interface RemoteInfo {
   name: string;
 }
 
-
 export type ConfigJson = {
   'min-ossl-version': number
   title: string
   description: string
+  keywords?: string[]
   database: string
   vis: 'chart.js'
 }
 
 export function basePath ({ owner, repo, branch, name }: RemoteInfo) {
   return `/api/collections/${encodeURIComponent(owner)}/${encodeURIComponent(repo)}/${encodeURIComponent(branch)}/${name}/`;
+}
+
+export function getGithubContentUrl ({ owner, repo, branch, name }: RemoteInfo) {
+  return `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/collections/${name}/`;
 }
